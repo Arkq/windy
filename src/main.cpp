70bb2ffbf32aebd4e27fdf64c8034021513d4ad5
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2015-11-29 23:54:26 +0100

    Allow static build on Windows

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,17 @@
 
 #include "Application.h"
 
+#if defined(Q_OS_WIN) && !defined(_DLL)
+// This sections allows static build on the Windows platform using MSVC++
+// compiler. During the non-DLL build the _DLL is not defined. In such a
+// case we will enforce plug-ins to be loaded, which in turn will generate
+// linking errors if one omits them.
+#include <QtPlugin>
+Q_IMPORT_PLUGIN(QSvgIconPlugin)
+Q_IMPORT_PLUGIN(QSvgPlugin)
+Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
+#endif // Q_OS_WIN && !_DLL
+
 int main(int argc, char *argv[]) {
 	Application app(argc, argv);
 	return app.exec();
